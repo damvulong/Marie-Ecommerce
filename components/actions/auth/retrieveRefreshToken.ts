e6277c736bd871748f47f3ec07d0cb8dc48fe58c
damvulong <kingdamlong1708@gmail.com>
2025-06-08 19:01:13 +0700

    Add OneEntry integration with token management and environment configuration

---
+++ components/actions/auth/retrieveRefreshToken.ts
@@ -0,0 +1,9 @@
+"use client";
+
+import { cookies } from "next/headers";
+
+export default async function retrieveRefreshToken() {
+  const cookieStore = await cookies();
+
+  return cookieStore.get("refresh_token")?.value;
+}
